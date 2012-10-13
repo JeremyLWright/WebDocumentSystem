c782880316774255e1ad117751aa6a70b97a36b4
Jeremy Wright <jwright@quiescent.us>
2012-10-12 23:24:37 -0700

    Added Partial pages for AJAX responses.

---
+++ WebDocumentSystem/WebDocumentSystem/Document/_DocumentMetaData.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="_DocumentMetaData.aspx.cs" Inherits="WebDocumentSystem.Document._DocumentMetaData" %>
+    <li class="nav-header">Document Notes</li>
+    <li><%= DateTime.Now %></li>
+    <li>Sweet Document Notes!</li>
---
+++ WebDocumentSystem/WebDocumentSystem/Scripts/DocumentHelper.js
@@ -0,0 +1,22 @@
+﻿function getDocumentListRowId(row) {
+    return row.cells[0].children[1].innerHTML;
+}
+
+
+function document_row_click(event) {
+    $('#DocumentNotes').load('/Document/_DocumentMetaData.aspx');
+
+    //alert('Processing Document: '+getDocumentListRowId(event));
+}
+
+function document_lock_click(event) {
+    getDocumentListRowId(event.parentNode);
+    if (eval(event.attributes["data-lock"].value)) {
+        event.attributes["data-lock"].value = false;
+        event.childNodes[0].attributes["src"].value = "../Images/glyphicons_204_unlock.png";
+    }
+    else {
+        event.attributes["data-lock"].value = true;
+        event.childNodes[0].attributes["src"].value = "../Images/glyphicons_203_lock.png";
+    }
+}
