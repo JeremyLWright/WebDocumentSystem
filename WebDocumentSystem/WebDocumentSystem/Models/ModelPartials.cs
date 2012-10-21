c07d87edd8925326abf6b6c744d774ff84e2e7d9
Jeremy <Jeremy@RedShift-Win.ph.cox.net>
2012-10-21 16:29:52 -0700

    Added Model Partials.

---
+++ WebDocumentSystem/WebDocumentSystem/Models/ModelPartials.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDocumentSystem.Models
+{
+    public partial class Document
+    {
+        public Document()
+        {
+            LastModified = DateTime.Now;
+        }
+    }
+
+    public partial class DocumentData
+    {
+        public DocumentData()
+        {
+            CreatedDate = DateTime.Now;
+        }
+    }
+
+    public partial class DocumentNote
+    {
+        public DocumentNote()
+        {
+            CreatedDate = DateTime.Now;
+        }
+    }
+
+    public partial class Audit_Log
+    {
+        public Audit_Log()
+        {
+            Date = DateTime.Now;
+        }
+    }
+}
